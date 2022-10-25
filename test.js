ab659994f78dee67e8b050fbdd9b136ac95579c1
tairet3 <tairet3@gmail.com>
2022-10-25 23:30:13 +0300

    v 3.0 Success

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
 
+
 (async function example() {
   let driver = await new Builder().forBrowser(Browser.CHROME).build();
   try {
@@ -7,10 +8,11 @@ const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
     let searchField = await driver.findElement(By.name('q'));
     await searchField.sendKeys('Денис Мархель');
     await searchField.sendKeys(Key.ENTER);
-    await driver.wait(until.titleIs('Денис Мархель - YouTube'),5000);
-    
-    let list = await driver.findElements(By.className('g'));
+    //await driver.wait(until.titleIs('Денис Мархель'),5000);
+
+    let list = await driver.findElements(By.className('MjjYud'));
     if(list.length <= 0){
+        console.log('Test - Fail');
         throw new Error ('Test - Fail');
     } else {
         console.log ('Test - Success');
