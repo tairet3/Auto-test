0311d8efdb97fe43bd7165ea68fad2f4a34e2885
tairet3 <tairet3@gmail.com>
2022-10-25 22:56:31 +0300

    v 2.0

---
+++ test.js
@@ -0,0 +1,22 @@
+const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
+
+(async function example() {
+  let driver = await new Builder().forBrowser(Browser.CHROME).build();
+  try {
+    await driver.get('https://www.google.com/ncr');
+    let searchField = await driver.findElement(By.name('q'));
+    await searchField.sendKeys('Денис Мархель');
+    await searchField.sendKeys(Key.ENTER);
+    await driver.wait(until.titleIs('Денис Мархель - YouTube'),5000);
+    
+    let list = await driver.findElements(By.className('g'));
+    if(list.length <= 0){
+        throw new Error ('Test - Fail');
+    } else {
+        console.log ('Test - Success');
+    }
+
+  } finally {
+    await driver.quit();
+  }
+})();
